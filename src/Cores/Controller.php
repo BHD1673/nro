5b71248236b68158e496c5421156e32684ed4bc6
BHD1673 <banhaiduong167@gmail.com>
2024-06-16 09:36:24 +0700

    Fixing and adding configure to file in core

--- src/Cores/Controller.php
+++ src/Cores/Controller.php
@@ -4,7 +4,7 @@ namespace Admin\Backend\Cores;
 
 use eftec\bladeone\BladeOne;
 
-class Controllers
+class Controller
 {   
     public function renderViewClient($view, $data = []) {
         $templatePath = __DIR__ . '/../Views/Client';
