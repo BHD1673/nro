0b48d904434e21decb9ee1e1fe9d8e376f39f294
BHD1673 <banhaiduong167@gmail.com>
2024-07-09 22:35:38 +0700

    add: basic helper like laravel

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 // PSR-4 autoloading configured. Use "namespace Admin\Backend;" in src/
 // Include the Composer autoloader with: require 'vendor/autoload.php';
 
-
+use Dotenv\Dotenv;
 
 require 'vendor/autoload.php';
 
-// This aint nothing much to see in this file.
+Dotenv::createImmutable(__DIR__)->load();
 
+require "helper.php";
+// This aint nothing much to see in this file. It just interact with the routes.
 require "routes/index.php";
