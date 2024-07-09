64354a77cbbc82b2a2975dbb492cf4bed26b512a
BHD1673 <banhaiduong167@gmail.com>
2024-07-09 18:05:40 +0700

    fix: category route to admin

--- routes/Admin.php
+++ routes/Admin.php
@@ -1,29 +1,60 @@
 <?php
+
+use Admin\Backend\Controllers\Admin\DashboardController;
+use Admin\Backend\Controllers\Admin\CategoryController;
 // Admin Routes
-$router->mount('/admin', function() use ($router) {
+// $router->mount('/admin', function() use ($router) {
+
+
+
+//     // CRUD: Bài viết
+//     $router->get('/bai-viet',                                           'AdminController@listPosts');
+//     $router->get('/bai-viet/them-moi',                                  'AdminController@createPostForm');
+//     $router->post('/bai-viet/them-moi',                                 'AdminController@createPost');
+//     $router->get('/bai-viet/sua/{id}',                                  'AdminController@editPostForm');
+//     $router->post('/bai-viet/sua/{id}',                                 'AdminController@updatePost');
+//     $router->post('/bai-viet/xoa/{id}',                                 'AdminController@deletePost');
+
+//     // Single Pages
+//     $router->get('/trang-chinh',                                        'AdminController@dashboard');
+//     $router->get('/stats/the-cao',                                      'AdminController@cardStats');
+//     $router->get('/stats/bank',                                         'AdminController@bankStats');
+//     $router->get('/config',                                             'AdminController@editConfig');
+//     $router->post('/config',                                            'AdminController@updateConfig');
+//     $router->get('/seo',                                                'AdminController@editSEO');
+//     $router->post('/seo',                                               'AdminController@updateSEO');
+// });
+
+// $router->before('GET|POST', '/admin/*.*', function() {
+//     if (! isset($_SESSION['user'])) {
+//         header('location: ' . url('login') );
+//         exit();
+//     }
+// });
+
+$router->mount('/admin', function () use ($router) {
+
+    $router->get('/',                                                   DashboardController::class . '@dashboard');
+
+    // // CRUD USER
+    // $router->mount('/users', function () use ($router) {
+    //     $router->get('/',                                            UserController::class . '@index');
+    //     $router->get('/create',                                      UserController::class . '@create');
+    //     $router->post('/store',                                      UserController::class . '@store');
+    //     $router->get('/{id}/show',                                   UserController::class . '@show');
+    //     $router->get('/{id}/edit',                                   UserController::class . '@edit');
+    //     $router->post('/{id}/update',                                UserController::class . '@update');
+    //     $router->get('/{id}/delete',                                 UserController::class . '@delete');
+    // });
 
     // CRUD: Danh mục bài viết
-    $router->get('/danh-muc',                                           'AdminController@listCategories');
-    $router->get('/danh-muc/them-moi',                                  'AdminController@createCategoryForm');
-    $router->post('/danh-muc/them-moi',                                 'AdminController@createCategory');
-    $router->get('/danh-muc/sua/{id}',                                  'AdminController@editCategoryForm');
-    $router->post('/danh-muc/sua/{id}',                                 'AdminController@updateCategory');
-    $router->post('/danh-muc/xoa/{id}',                                 'AdminController@deleteCategory');
+    $router->mount('/danh-muc', function() use ($router) {
+        $router->get('/trang/{id}',                                             CategoryController::class . '@index');             // Hiển thị danh sách danh mục
+        $router->post('/them-moi',                                              CategoryController::class . '@create');            // Xử lý dữ liệu vào database
+        $router->get('/sua/{id}',                                               CategoryController::class . '@editFetch');         // Hiển thị form để sửa dữ liệu
+        $router->post('/cap-nhat/{id}',                                         CategoryController::class . '@editPost');          // Gửi dữ liệu sửa
+        $router->get('/xoa/{id}',                                               CategoryController::class . '@delete');            // Xoá dữ liệu
+    });
+    
 
-    // CRUD: Bài viết
-    $router->get('/bai-viet',                                           'AdminController@listPosts');
-    $router->get('/bai-viet/them-moi',                                  'AdminController@createPostForm');
-    $router->post('/bai-viet/them-moi',                                 'AdminController@createPost');
-    $router->get('/bai-viet/sua/{id}',                                  'AdminController@editPostForm');
-    $router->post('/bai-viet/sua/{id}',                                 'AdminController@updatePost');
-    $router->post('/bai-viet/xoa/{id}',                                 'AdminController@deletePost');
-
-    // Single Pages
-    $router->get('/trang-chinh',                                        'AdminController@dashboard');
-    $router->get('/stats/the-cao',                                      'AdminController@cardStats');
-    $router->get('/stats/bank',                                         'AdminController@bankStats');
-    $router->get('/config',                                             'AdminController@editConfig');
-    $router->post('/config',                                            'AdminController@updateConfig');
-    $router->get('/seo',                                                'AdminController@editSEO');
-    $router->post('/seo',                                               'AdminController@updateSEO');
 });
